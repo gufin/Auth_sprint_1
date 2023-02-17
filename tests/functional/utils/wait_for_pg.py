5dde10cec62b460228fbcc69b91dd27f4f41f004
Дробышев Иван <gufinhaly@gmail.com>
2023-02-17 19:07:12 +0300

    swagger added, test corrections

---
+++ tests/functional/utils/wait_for_pg.py
@@ -0,0 +1,33 @@
+import os
+import socket
+import time
+
+import backoff
+
+from settings import app_settings
+from utils.helpers import backoff_handler
+
+
+@backoff.on_exception(
+    backoff.expo,
+    (ConnectionError,),
+    on_backoff=backoff_handler,
+    max_time=app_settings.CONNECTIONS_MAX_TIME,
+)
+def pg_connect():
+    pg_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            pg_socket.connect(
+                (os.environ["POSTGRES_HOST"], int(os.environ["POSTGRES_PORT"]))
+            )
+            pg_socket.close()
+            break
+        except ConnectionError as ex:
+            time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    print("Попытка установить соединение с postgres")
+    pg_connect()
+    print("Соединение установлено успешно")
